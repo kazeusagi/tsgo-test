b69742924b59524cf6ec56497bc5738bd19455ce
kazeusagi <toshiki1098@gmail.com>
2025-05-25 11:12:57 +0000

    feat: add many packages

---
+++ packages/ui4/src/button.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import type { ReactNode } from 'react';
+
+interface ButtonProps {
+	children: ReactNode;
+	className?: string;
+	appName: string;
+}
+
+export const Button = ({ children, className, appName }: ButtonProps) => {
+	return <></>;
+};
